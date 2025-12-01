3bb9e1d03e7a9188b75956657fd528452f376853
0xVantrex <callagansikuche0@gmail.com>
2025-12-01 12:09:57 +0300

    addfunctions

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def service(request):
+    return render(request,'service.html')
+def event(request):
+    return render(request,'event.html')
+def team(request):
+    return render(request,'team.html')
+def contact(request):
+    return render(request,'contact.html')
+def donation(request):
+    return render(request,'donation.html')
+def testimonial(request):
+    return render(request,'testimonial.html')
+def features(request):
+    return render(request,'features.html')
